9f92b5ca02005a71dc0379571e9dd46646f46c9a
Abel Xu <yp_xu@outlook.com>
2018-11-09 08:52:58 -0600

    Initial Commit

---
+++ GG Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GG Todo
+//
+//  Created by Abel Xu on 11/9/18.
+//  Copyright © 2018 Yupeng Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
